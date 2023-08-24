fa90902f2630d242423cb9a07ab772deb4778227
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2023-08-24 10:24:59 +0000

    Update_terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,51 @@
-import { useState  } from 'react'
+import { useState} from 'react'
 import './App.less'
 
 function App() {
   const [buttons ,buttonuse] = useState("")
-  
-  const CodeKeyboard= (event: React.KeyboardEvent) => {
-    
-    console.log(event.code);
-    if(event.code === "KeyQ"){
-      alert("Q")
-    }
-  };
+  const [Volume, Volumechange] = useState("")
 
-  const CodeKeyboardnt= (event: React.KeyboardEvent) => {
-    
-    console.log(event.code);
-    if(event.code === "KeyQ"){
-      alert("Q")
-      
-     
-    }
-  };
-
+  const Pressedk = function(name:string,selected:string){
+    const audio = new Audio(name)
+    buttonuse(selected)
+    audio.volume = parseInt(Volume)/10;
+    audio.play()
+  }
  
   return (
-    <div className='bodys'   onKeyUp={CodeKeyboardnt} onKeyDown={CodeKeyboard} >
+    <div className='bodys' >
       <div className='padsbody'>
         <section className='pads'  >
         
-            <button id="Q"  tabIndex={0}>Q</button>
-            <button>W</button>
-            <button>E</button>
+            <button onClick={() => Pressedk("../sounds/Heater-1.mp3","Heater-1")} tabIndex={0}>Q</button>
+            <button onClick={()=>Pressedk("../sounds/Heater-2.mp3","Heater-2")}>W</button>
+            <button onClick={()=>Pressedk("../sounds/Heater3.mp3","Heater3")}>E</button>
       
-            <button>A</button>
-            <button>S</button>
-            <button>D</button>
+            <button onClick={()=>Pressedk("../sounds/Heater-4_1.mp3","Heater-4_1")}>A</button>
+            <button onClick={()=>Pressedk("../sounds/Heater-6.mp3","Heater-6")}>S</button>
+            <button onClick={()=>Pressedk("../sounds/Cev_H2.mp3","Cev_H2")}>D</button>
         
-            <button>Z</button>
-            <button>X</button>
-            <button>C</button>
-          
+            <button onClick={()=>Pressedk("../sounds/Dsc_Oh.mp3","Dsc_Oh")}>Z</button>
+            <button onClick={()=>Pressedk("../sounds/Kick_n_Hat.mp3","Kick_n_Hat")}>X</button>
+            <button onClick={()=>Pressedk("../sounds/RP4_KICK_1.mp3","RP4_KICK_1")}>C</button>
+
+            <input type="range" value={Volume} onChange={(e)=>Volumechange(e.target.value)} min="0" max="10"/>
+            
+            
+            {Number.isNaN(parseInt(Volume)*10) === true&&
+            <h1>0🔈</h1>
+            }
+
+            {Number.isNaN(parseInt(Volume)*10)===false &&
+            <h1>{parseInt(Volume)*10}🔊</h1>
+            }
+            
+            
+
         </section>
+        <div className='Names'>
+              <h1>{buttons}</h1>
+            </div>
       </div>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.less'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <div className='generalbody'>
-  <React.StrictMode>
+  <div>
     
     <App />
     
-  </React.StrictMode>
+  </div>
   </div>
 )
